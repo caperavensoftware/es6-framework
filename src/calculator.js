97af1a3127fd936ee2ff996945bb0f5a41020e49
caperaven <caperaven@icloud.com>
2016-05-11 18:57:38 +0200

    first commit

---
+++ src/calculator.js
@@ -0,0 +1,20 @@
+export class Calculator {
+    add(value1, value2) {
+        return value1 + value2;
+    }
+    
+    subtract(value1, value2) {
+        return value1 - value2;       
+    }
+    
+    multiply(value1, value2) {
+        const result = value1 * value2;
+        return result;
+    }
+}
+
+export default class Something {
+    report() {
+        console.log(report);
+    }
+}
